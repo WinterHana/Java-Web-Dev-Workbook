61cf984aa9fd732e141c49fe3ace664b08b8b195
Winterhana <haha920@naver.com>
2026-05-18 22:05:48 +0900

    feat : study

--- springex/src/main/java/org/zerock/springex/sample/SampleService.java
+++ springex/src/main/java/org/zerock/springex/sample/SampleService.java
@@ -2,7 +2,6 @@ package org.zerock.springex.sample;
 
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
